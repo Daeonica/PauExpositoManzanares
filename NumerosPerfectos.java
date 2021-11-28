6eb8a18d987e72ff915eacd6c4f3906f1a68b2b6
Pau Expósito <95183640+Daeonica@users.noreply.github.com>
2021-11-28 19:05:06 +0100

    NúmerosPerfectos
    Programa para encontrar números perfectos

---
+++ NumerosPerfectos.java
@@ -0,0 +1,20 @@
+
+public class Ej05 {
+     public static void main(String[] args) {
+        int num, i, suma=0, perf=0;
+        System.out.println("First 3 perfect numbers: ");
+        for(num=1;perf <3;num++){      // num es el número que vamos a analizar para ver si es o no perfecto.
+            //creamos un bucle for que tiene por condición parar cuando tengamos 3 números perfectos. Hasta entonces irá analizando números ya que en cada iteración a num le suma 1.
+            for(i = 1;i < num;i++){    // i son los divisores. Repetiremos el buche hasta que i llegue a num.                     
+                 if(num % i==0){//miramos los divisores
+                    suma = suma + i; // si es divisor se suma
+                 }
+            }
+          if(num == suma){             // si el número es igual a la suma de sus divisores es perfecto              
+             System.out.println(num);
+             perf++;//en tal caso le sumamos +1 al contador de números perfectos
+          }
+        }
+    }
+
+}
